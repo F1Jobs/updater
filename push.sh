587cd0cb75d7a22d83ed9aa108054be2b4f6bb3d
Ayush <itsayushdwivedi@gmail.com>
2018-08-17 09:10:48 +0530

    Add echo statements

--- push.sh
+++ push.sh
@@ -21,25 +21,38 @@
 
 # Get to the updater directory inside updater repo
 cd /root/f1jobs/updater/updater
+echo "Current directory - Updater"
+
+# Pull changes from remote
+echo "Pulling changes from remote"
+git pull --rebase origin master
 
 # Run the updater script
+echo "Running the updater script"
 python3 getjobs.py
+echo "Updater script finished running"
 
 # Get to the client directory
 cd /root/f1jobs/client
+echo "Current directory - Client"
 
-# Pull changes from the remote
+# Pull changes from remote
+echo "Pulling changes from remote"
 git pull --rebase origin master
 
 # Check if there is something to be committed, if yes, stage the changes,
 # otherwise terminate indicating success
+echo "Checking status"
 if [[ $(git status --porcelain | head -c1 | wc -c) -ne 0 ]]; then
+    echo "Found changes. Staging now"
     git add --all
 else
+    echo "No changes found. Exiting..."
     exit 0
 fi
 
 # Commit the changes
+echo "Commiting the changes"
 git commit -m "Site updated"
 
 # A fancy 1-liner that can replace the above two sections if you don't care
@@ -47,4 +60,6 @@ git commit -m "Site updated"
 # git diff --exit-code --no-patch && exit 0 || git commit -a -m "Site updated"
 
 # Push to remote
+echo "Pushing changes to remote"
 git push origin master
+echo "Changes pushed. Exiting..."
